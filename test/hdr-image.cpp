40bc108fcf958fb3e8b42697f75999995e3591ce
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-03-17 10:56:54 +0100

    validCoordinates and HdrImage constructor test

--- test/hdr-image.cpp
+++ test/hdr-image.cpp
@@ -1,3 +1,21 @@
+#include "hdr-image.h"
+#undef NDEBUG
+#include <cassert>
+
 int main() {
+	HdrImage img(7, 4);
+
+	// Test constructor
+	assert(img.width == 7);
+	assert(img.height == 4);
+
+	// Test validCoordinates
+	assert(img.validCoordinates(0, 0));
+	assert(img.validCoordinates(6, 3));
+	assert(!img.validCoordinates(7, 3));
+	assert(!img.validCoordinates(6, 4));
+	assert(!img.validCoordinates(-1, 3));
+	assert(!img.validCoordinates(6, -1));
+
 	return 0;
 }
