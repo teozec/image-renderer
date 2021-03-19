4cb28c7e46af1cc3caf5eb0febaef5382c024f97
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-03-19 17:56:26 +0100

    Make Color methods inline

--- src/color.cpp
+++ src/color.cpp
@@ -1,34 +1,17 @@
 #include <cmath>
 #include "color.h"
 
-#define EPSILON 1e-10f
-
-Color Color::operator+(const Color other) {
-	return Color{r+other.r, g+other.g, b+other.b};
-}
-
-Color Color::operator*(float c) {
-	return Color{c*r, c*g, c*b};
-}
-
-Color Color::operator*(const Color other) {
-	return Color{other.r*r, other.g*g, other.b*b};
-}
-
-static bool areClose(float a, float b, float epsilon) {
+static bool areClose(const float a, const float b, const float epsilon) {
 	return fabs(a-b) < epsilon;
 }
 
-bool areColorsClose(const Color a, const Color b, float epsilon) {
+bool areColorsClose(const Color a, const Color b, const float epsilon) {
 	return areClose(a.r, b.r, epsilon) and
 		areClose(a.g, b.g, epsilon) and
 		areClose(a.b, b.b, epsilon);
 }
 
 bool Color::operator==(const Color other) {
-	return areColorsClose(*this, other, EPSILON);
-}
-
-bool Color::operator!=(const Color other) {
-	return !(*this == other);
+	const float epsilon = 1e-10f;
+	return areColorsClose(*this, other, epsilon);
 }
