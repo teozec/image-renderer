d600da66810a18aa395eddff26c437ff05f2435f
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-04-14 12:14:46 +0200

    Implement Point==Point and std::string(Point)

--- include/geometry.h
+++ include/geometry.h
@@ -19,10 +19,13 @@ along with image-renderer.  If not, see <https://www.gnu.org/licenses/>. */
 #define GEOMETRY_H
 
 #include <vector>
+#include <string>
+#include <sstream>
 
 template <typename In1, typename In2, typename Out> Out _sum(const In1 &a, const In2 &b) {
 	return Out{a.x + b.x, a.y + b.y, a.z + b.z};
 }
+template <typename T> bool areClose(const T &a, const T &b, float epsilon);
 
 struct Vec{
     float x, y, z;
@@ -39,11 +42,26 @@ struct Point {
 	Point(const Point &) = default;
 	Point(Point &&) = default;
 
-	Point operator-() {
+	Point operator-() const {
 		return Point{-x, -y, -z};
 	}
+
 	Vec operator-(const Point &other) {
-		_sum<Point, Point, Vec>(*this, -other);
+		return _sum<Point, Point, Vec>(*this, -other);
+	}
+
+	// Compare Points with a default precision.
+	// For a different precision, call areClose<Point> directly.
+	bool operator==(const Point &other) {
+		const float epsilon = 1e-10f;
+		return areClose<Point>(*this, other, epsilon);
+	}
+
+	// Convert Point to a human readable string with the values of its elements
+	operator std::string() const {
+		std::ostringstream ss;
+		ss << "Point(x=" << x << ", y=" << y << ", z=" << z;
+		return ss.str();
 	}
 
 };
--- src/geometry.cpp
+++ src/geometry.cpp
@@ -1,1 +1,12 @@
+#include <cmath>
 #include "geometry.h"
+
+static bool areClose(const float a, const float b, const float epsilon) {
+	return fabs(a-b) < epsilon;
+}
+
+template <typename T> bool areClose(const T &a, const T &b, float epsilon) {
+	return areClose(a.x, b.x, epsilon) and
+		areClose(a.y, b.y, epsilon) and
+		areClose(a.z, b.z, epsilon);
+}
