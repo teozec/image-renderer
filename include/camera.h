ca6f04ab089eddfe735291860260a6900219ffd2
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-04-28 11:19:58 +0200

    Implement Ray struct

--- include/camera.h
+++ include/camera.h
@@ -1,4 +1,61 @@
+/* Copyright (C) 2021 Luca Nigro and Matteo Zeccoli Marazzini
+
+This file is part of image-renderer.
+
+image-renderer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+image-renderer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with image-renderer.  If not, see <https://www.gnu.org/licenses/>. */
+
 #ifndef CAMERA_H
 #define CAMERA_H
+
+#include <limits>
+#include "geometry.h"
+
+struct Ray {
+	Point origin;
+	Vec dir;
+	float tmin, tmax;
+	int depth;
+
+	Ray(Point origin = Point(), Vec dir = Vec(), int depth = 0, float tmin = 1e-5, float tmax = std::numeric_limits<float>::infinity()) :
+		origin{origin}, dir{dir}, depth{depth}, tmin{tmin}, tmax{tmax} {}
+
+	bool isClose(Ray other, float epsilon) {
+		return areClose<Point>(origin, other.origin, epsilon) and
+			areClose<Vec>(dir, other.dir, epsilon);
+	}
+
+	bool operator==(Ray other) {
+		return origin == other.origin and dir == other.dir;
+	}
+
+	bool operator!=(Ray other) {
+		return !(*this == other);
+	}
+
+	/**
+	 * Return the Point corresponding to the Ray at the affine parameter t
+	 *
+	 * @param t The value of the affine parameter to evaluate the Ray at
+	 * @return The Point at t
+	 */
+	Point operator()(float t) {
+		return origin + dir * t;
+	}
+};
+
+Ray operator*(Transformation tr, Ray ray) {
+	return Ray(tr * ray.origin, tr * ray.dir, ray.depth, ray.tmin, ray.tmax);
+}
 
 #endif // CAMERA_H
---
+++ test/camera.cpp
@@ -0,0 +1,47 @@
+/* Copyright (C) 2021 Luca Nigro and Matteo Zeccoli Marazzini
+
+This file is part of image-renderer.
+
+image-renderer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+image-renderer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with image-renderer.  If not, see <https://www.gnu.org/licenses/>. */
+
+#include "camera.h"
+#include "geometry.h"
+#undef NDEBUG
+#include <cassert>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	// Test Ray
+	Ray ray1{Point{1.f, 2.f, 3.f}, Vec{5.f, 4.f, -1.f}};
+	Ray ray2{Point{1.f, 2.f, 3.f}, Vec{5.f, 4.f, -1.f}};
+	Ray ray3{Point{5.f, 1.f, 4.f}, Vec{3.f, 9.f, 4.f}};
+	assert(ray1 == ray2);
+	assert(ray1 != ray3);
+
+	Ray ray4{Point{1.f, 2.f, 4.f}, Vec{4.f, 2.f, 1.f}};
+	assert(ray4(0.f) == ray4.origin);
+	assert((ray4(1.f) == Point{5.f, 4.f, 5.f}));
+	assert((ray4(2.f) == Point{9.f, 6.f, 6.f}));
+
+	Ray ray5{Point{1.f, 2.f, 3.f}, Vec{6.f, 5.f, 4.f}};
+	Transformation tr = translation(Vec{10.f, 11.f, 12.f}) * rotationX(M_PI/2);
+	Ray transformed = tr * ray5;
+	assert((transformed.origin == Point{11.f, 8.f, 14.f}));
+	assert((transformed.dir == Vec{6.f, -4.f, 5.f}));
+
+	return 0;
+}
