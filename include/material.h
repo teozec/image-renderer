b1ecd32a845d8d24416924f1c25fe860f31ae28f
Luca Nigro <luca.nigro@studenti.unimi.it>
2021-05-19 10:57:10 +0200

    Implementation abstract Pigment struct

--- include/material.h
+++ include/material.h
@@ -23,6 +23,28 @@ along with image-renderer.  If not, see <https://www.gnu.org/licenses/>. */
 #include "color.h"
 #include "geometry.h"
 #include "shape.h"
+#include "hdr-image.h"
+#include "shape.h"
+
+struct Pigment {
+	virtual Color getColor(Vec2D coords) = 0;
+};
+
+struct UniformPigment : public Pigment {
+	Color color{};
+
+	UniformPigment() : Pigment() {}
+	UniformPigment(Color color) : Pigment(), color{color} {}
+
+	Color operator()(Vec2D coords) {
+		return color;
+	}
+};
+/*
+struct CheckeredPigment : public Pigment {
+
+}
+*/
 
 struct BRDF {
 	std::shared_ptr<Pigment> pigment;
