e0d1f8f2435186ab4ac6bf75b930fb4d45e13241
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-05-24 12:45:07 +0200

    Add Sphere test

--- test/shape.cpp
+++ test/shape.cpp
@@ -64,6 +64,17 @@ void testSphere()
 	assert((areSphereSurfacePointsClose(hit2.surfacePoint, Vec2D{0.5f, 0.f})));
 	assert(areClose(hit2.t, 2.f));
 	assert(hit2.ray == ray2);
+	
+	Sphere sphere3;
+	Ray ray3{Point{7.f, 0.f, 0.f}, Vec{-2.f, 0.f, 0.f}};
+	HitRecord hit3{sphere3.rayIntersection(ray3)};
+	assert(hit3.hit);
+	assert((hit3.worldPoint == Point{1.f, 0.f, 0.f}));
+	assert((hit3.normal == Normal{1.f, 0.f, 0.f}));
+	assert((areSphereSurfacePointsClose(hit3.surfacePoint, Vec2D{0.5f, 0.f})));
+	assert(areClose(hit3.t, 3.f));
+	assert(hit3.ray == ray3);
+
 }
 
 void testSphereInner()
