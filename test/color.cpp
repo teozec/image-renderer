cb40f5cb803107ee8fe35ed3df7404dea49abaeb
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-06-04 10:49:04 +0200

    Add Color::operator/() test

--- test/color.cpp
+++ test/color.cpp
@@ -23,35 +23,46 @@ using namespace std;
 
 int main()
 {
-	Color c1{1.0, 2.0, 3.0};
-	Color c2{5.0, 7.0, 9.0};
+	Color c1{1.f, 2.f, 3.f};
+	Color c2{5.f, 7.f, 9.f};
 
 	assert(c1==c1);
 	assert(!(c1!=c1));
 	assert(!(c1==c2));
 	assert(c1!=c2);
 
-	assert((c1+c2)==(Color{6.0, 9.0, 12.0}));
+	assert((c1+c2)==(Color{6.f, 9.f, 12.f}));
 	
-	assert((c1*1.0)==c1);
-	assert((c1*5.0)!=c2);
-	assert(!((c1*2.0)==c1));
-	assert((c2*3.0)==(Color{15.0, 21.0, 27.0}));
+	assert((c1*1.f)==c1);
+	assert((c1*5.f)!=c2);
+	assert(!((c1*2.f)==c1));
+	assert((c2*3.f)==(Color{15.f, 21.f, 27.f}));
 
-	assert((c1*c2)==(Color{5.0, 14.0, 27.0}));
+	assert((c1/1.f)==c1);
+	assert((c1/5.f)!=c2);
+	assert(!((c1/2.f)==c1));
+	assert((c2/2.f)==(Color{2.5f, 3.5f, 4.5f}));
 
-	Color c3{1e-11, 2e-11, 3e-11};
-	Color c4{5e-11, 7e-11, 9e-11};
+	assert((c1*c2)==(Color{5.f, 14.f, 27.f}));
+
+	c1 *= 2.f;
+	assert((c1 == Color{2.f, 4.f, 6.f}));
+
+	c2 /= 2.f;
+	assert((c2==Color{2.5f, 3.5f, 4.5f}));
+
+	Color c3{1e-11f, 2e-11f, 3e-11f};
+	Color c4{5e-11f, 7e-11f, 9e-11f};
 
 	assert(c3==c4);
 	assert(!(c3!=c4));
-	assert(!areColorsClose(c1, c3, 1e-12));
+	assert(!areColorsClose(c1, c3, 1e-12f));
 
-	Color c5{1.0, 2.0, 3.0};
-	Color c6{9.0, 5.0, 7.0};
+	Color c5{1.f, 2.f, 3.f};
+	Color c6{9.f, 5.f, 7.f};
 
-	assert(c5.luminosity() == 2.0);
-	assert(c6.luminosity() == 7.0);
+	assert(c5.luminosity() == 2.f);
+	assert(c6.luminosity() == 7.f);
 
 	return 0;
 }
