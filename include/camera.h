361b7adb2dc740e29dcd66fc490cc45dc6f244b0
Luca Nigro <luca.nigro@studenti.unimi.it>
2021-07-06 16:15:01 +0200

    Less parallelization

--- include/camera.h
+++ include/camera.h
@@ -24,6 +24,7 @@ along with image-renderer.  If not, see <https://www.gnu.org/licenses/>. */
 #include "hdr-image.h"
 #include "color.h"
 #include "random.h"
+#include <omp.h>
 
 struct Ray {
 	Point origin;
@@ -190,13 +191,14 @@ struct ImageTracer {
 	 * @param color The function to compute a Color given a Ray
 	 */
 	template <typename T> void fireAllRays(T colorFunc) {
-		for (int row{}; row < image.height; row++) {
-			for (int col{}; col < image.width; col++) {
-				std::cout << "\rRendering: " << (int)((col+row*image.width)*100.f/(image.height*image.width)) << "% " << std::flush;
+		for (int row = 0; row < image.height; row++) {
+			std::cout << "\rRendering: " << (int)(row*100.f/image.height) << "% " << std::flush;
+			for (int col = 0; col < image.width; col++) {
 				Color cumColor{0.f, 0.f, 0.f};
 				if (samplesPerSide > 0) {
-					for (int rowPixel{}; rowPixel<samplesPerSide; rowPixel++) {
-						for (int colPixel{}; colPixel<samplesPerSide; colPixel++) {
+					#pragma omp parallel for collapse(2)
+					for (int rowPixel = 0; rowPixel<samplesPerSide; rowPixel++) {
+						for (int colPixel = 0; colPixel<samplesPerSide; colPixel++) {
 							float uPixel = (colPixel+pcg.randFloat())/samplesPerSide;
 							float vPixel = (rowPixel+pcg.randFloat())/samplesPerSide;
 							Ray ray = fireRay(col, row, uPixel, vPixel);
