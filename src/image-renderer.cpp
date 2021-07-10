e5694d818c923ebf365fb3cb4d325e174c3f98ea
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2021-07-10 19:53:36 +0200

    Add --dryRun render option to verify scenefiles without rendering

--- src/image-renderer.cpp
+++ src/image-renderer.cpp
@@ -94,6 +94,7 @@ along with image-renderer.  If not, see <https://www.gnu.org/licenses/>. */
 	"General options:" << endl << \
 	"	-h, --help							Print this message." << endl << \
 	"	-q, --quiet							Do not show rendering progress." << endl << \
+	"	-D, --dryRun							Parse scenefile, but do not render image. Useful to check correctness of scenes." << endl << \
 	"	-f <variable1:value1,variable2:value2,...>, --float=<...>	Define float variables to be used in the scenefile." << endl << \
 	"	-w <value>, --width=<value>					Width of the final image (default 640)." << endl << \
 	"	-h <value>, --height=<value>					Height of the final image (default 480)." << endl << \
@@ -461,6 +462,10 @@ int render(argh::parser cmdl)
 		HdrImage image{width, height};
 		ImageTracer tracer{image, *scene.camera, samplesPerSide};
 		PCG pcg{(uint64_t) seed};
+
+		if (cmdl[{"-D", "--dryRun"}])
+			return 0;
+
 		if (renderer == "path")
 			tracer.fireAllRays(PathTracer{scene.world, pcg, nRays, depth, roulette}, verbose);
 		else if (renderer == "debug")
--- tools/bash-completion.bash
+++ tools/bash-completion.bash
@@ -189,7 +189,7 @@ _complete_image_renderer()
 
 			# Complete double dash arguments
 			elif [[ "${cur}" == --* ]]; then
-				COMPREPLY=($(compgen -W "--help --quiet --width= --height= --aspectRatio= --seed= --antialiasing= --renderer= --outfile= --nRays= --depth= --roulette= --float=" -- $cur))
+				COMPREPLY=($(compgen -W "--help --quiet --width= --height= --dryRun --aspectRatio= --seed= --antialiasing= --renderer= --outfile= --nRays= --depth= --roulette= --float=" -- $cur))
 				# Remove space if there is a "=" in completion
 				if [[ "${COMPREPLY[@]}" =~ "=" ]]; then
 					compopt -o nospace
@@ -197,7 +197,7 @@ _complete_image_renderer()
 
 			# Complete single dash arguments
 			elif [[ "${cur}" == -* ]]; then
-				COMPREPLY=($(compgen -W "-q -w -h -a -s -A -R -o -n -d -r -f" -- $cur))
+				COMPREPLY=($(compgen -W "-q -w -h -D -a -s -A -R -o -n -d -r -f" -- $cur))
 
 			# Complete input filename
 			else
